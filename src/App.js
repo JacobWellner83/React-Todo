f1247e1b17acf1756d1c0d38f17f69d44ad21611
JacobWellner83 <jcwellner83@gmail>
2019-05-14 20:26:07 -0500

    class App constructor for todos written

--- src/App.js
+++ src/App.js
@@ -3,6 +3,25 @@ import TodoList from './components/TodoComponents/TodoList';
 import TodoForm from './components/TodoComponents/TodoForm';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: [
+        {
+          task: '',
+          id: 0,
+          completed: false
+        },
+        {
+          task: '',
+          id: 0,
+          completed: false
+        }
+      ],
+      todo: ''
+    };
+  }
+  
   render() {
     return (
       <div>
