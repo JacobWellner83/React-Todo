7cc9d653b097e1088059fc3391f91866d14fd70b
JacobWellner83 <jcwellner83@gmail>
2019-05-16 15:59:54 -0500

    Apps constructor and render done

--- src/App.js
+++ src/App.js
@@ -6,16 +6,31 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-     
+     task: '',
+     todos: [
+       {
+         task: 'construct Todo list',
+         id: Date.now(),
+         completed: false,
+       }
+     ]
     };
   }
   
   render() {
     return (
       <div>
+        <h1>Todo List</h1>
+        <TodoForm
+          task={this.state.task}
+          addTodoHandler={this.addTodoHandler}
+          removeTodoHandler={this.removeTodoHandler}
+          handleChange={this.handleChange}
+        />
         <TodoList
-        handleTodoComplete={this.changeTodo}
-
+          todos={this.state.todos}
+          handleToggleCompleted={this.handleToggleCompleted}
+        />
     );
   }
 }
